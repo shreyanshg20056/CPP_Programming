35f0dc9f4b29dfcece9630e8d856b5618dbb5044
shreyanshg20056 <shreyanshg2006@gmail.com>
2026-09-06 01:05:55 +0530

    Learning Codes
    Arrays Learning Codes

--- Arrays/arrays_2.cpp
+++ Arrays/arrays_2.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int i,arr[5];
+    float max = arr[0];
     
+    for(i=0;i<5;i++){
+        cin>>arr[i];
+    }
+
+    for(i=0;i<5;i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+
+    cout<<"Max:"<<max<<endl;
     return 0;
 }
